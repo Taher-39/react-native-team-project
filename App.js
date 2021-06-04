ed780bc4020de282eb5821db4d61137bc73609b9
Abu Taher <abut70243@gmail.com>
2021-06-04 09:43:05 +0600

    first text chhanges

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>This text from taher</Text>
       <StatusBar style="auto" />
     </View>
   );
